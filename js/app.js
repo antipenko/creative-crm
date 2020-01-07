52262d5d0e55ca96c7bb929e80042b50b0b1e2fb
Vlad Antipenko <vlad.antipenko@namecheap.com>
2020-01-07 11:57:02 +0200

    mobile header. add toolbar

---
+++ js/app.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    $('.ba-menu-btn').on('click', function(){
+        let menu = $('.ba-menu');
+
+        if (menu.hasClass('open')){
+            menu.removeClass('open').hide();
+        } else {
+            menu.addClass('open').show();
+        }
+    });
+
+    $(window).on('resize', function() {
+        if ($(this).width() > 991) {
+            $('.ba-menu').addClass('open').show();
+        } else {
+            $('.ba-menu').removeClass('open').hide();
+        }
+    })
+
+    $('#showSearch').on('click', function() {
+        let searchForm = $('.ba-search-form');    
+        searchForm.toggleClass('open');
+    })
+
+
+})
